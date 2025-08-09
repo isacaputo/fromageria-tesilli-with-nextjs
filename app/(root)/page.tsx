57c4fda0e27bc00a8157e4e6f31cebb87a8978b3
isacaputo <isadora.caputo@gmail.com>
2025-08-09 17:50:23 +0200

    feat: log sample data in Homepage component for debugging purposes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
-import { Button } from '@/components/ui/button';
+import sampleData from '@/db/sample-data';
 
 const Homepage = () => {
+  console.log('sample data', sampleData);
   return <></>;
 };
 
