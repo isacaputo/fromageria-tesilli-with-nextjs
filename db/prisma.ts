0637d1f02ce0ca07c374323301c70cb3b256fa6c
isacaputo <isadora.caputo@gmail.com>
2025-09-01 18:43:51 +0200

    fix: simplify PrismaClient result transformer by removing type annotations for product

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,23 +1,23 @@
-import { PrismaClient } from '@prisma/client';
 import { PrismaNeon } from '@prisma/adapter-neon';
-import { Product } from '@/types';
+import { PrismaClient } from '@prisma/client';
 
 const connectionString = process.env.DATABASE_URL!;
 
-// Instantiates the Prisma adapter using the Neon connection pool to handle the connection between Prisma and Neon.
+// Instantiates the Prisma adapter using the Neon connection pool
 const adapter = new PrismaNeon({ connectionString });
 
 // Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
+// @ts-expect-error - Adapter type compatibility issue
 export const prisma = new PrismaClient({ adapter }).$extends({
   result: {
     product: {
       price: {
-        compute(product: Product) {
+        compute(product) {
           return product.price.toString();
         },
       },
       rating: {
-        compute(product: Product) {
+        compute(product) {
           return product.rating.toString();
         },
       },
