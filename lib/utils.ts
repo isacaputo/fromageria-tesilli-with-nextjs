0bafcf14b400ab1a7e2199048ee4b10015e93207
isacaputo <isadora.caputo@gmail.com>
2025-09-01 18:14:28 +0200

    fix: correct formatting logic in formatNumberWithDecimal function

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,3 +9,9 @@ export function cn(...inputs: ClassValue[]) {
 export function convertToPlainObject<T>(prismaObject: T): T {
   return JSON.parse(JSON.stringify(prismaObject));
 }
+
+// Format number with decimal places
+export function formatNumberWithDecimal(num: number): string {
+  const [int, decimal] = num.toString().split('.');
+  return decimal ? `${int}.${decimal.padEnd(2, '0')}` : `${int}.00`;
+}
